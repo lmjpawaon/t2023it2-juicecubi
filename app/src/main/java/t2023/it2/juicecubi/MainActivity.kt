d13047d10c10e608e9f5e9ea759b336f6aa2f1f5
louispawaon <lpawaon@gmail.com>
2023-11-14 22:10:01 +0800

    chore: test firebase connection, working - check in logcat

--- app/src/main/java/t2023/it2/juicecubi/MainActivity.kt
+++ app/src/main/java/t2023/it2/juicecubi/MainActivity.kt
@@ -2,10 +2,36 @@ package t2023.it2.juicecubi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Test Firestore connection by retrieving data
+        retrieveDataFromFirestore()
+    }
+
+    private fun retrieveDataFromFirestore() {
+        db.collection("menu")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    // Enumerate all fields in the document
+                    for ((field, value) in document.data) {
+                        Log.d("FirestoreTest", "Document ID: ${document.id}, Field: $field, Value: $value")
+
+//                        // Update TextView with retrieved data
+//                        textView.append("$field: $value\n")
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("FirestoreTest", "Error getting documents: ", exception)
+            }
     }
 }
