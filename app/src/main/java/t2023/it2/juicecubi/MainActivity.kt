8706220a7c211f095bfded9f278198f6dbca7c2f
louispawaon <lpawaon@gmail.com>
2023-11-14 14:09:40 +0800

    initial commit

---
+++ app/src/main/java/t2023/it2/juicecubi/MainActivity.kt
@@ -0,0 +1,11 @@
+package t2023.it2.juicecubi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
