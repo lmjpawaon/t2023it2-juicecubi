5a3d1f255089a31b51c807f76e5e954018846ffd
Louis Miguel Jaboc Pawaon <lmjpawaon@addunet.ph>
2023-11-15 12:54:10 +0800

    chore: code for automated renaming of fieldnames

--- app/src/main/java/t2023/it2/juicecubi/MainActivity.kt
+++ app/src/main/java/t2023/it2/juicecubi/MainActivity.kt
@@ -4,34 +4,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class MainActivity : AppCompatActivity() {
     private val db = FirebaseFirestore.getInstance()
+    private val collectionName = "menu"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Test Firestore connection by retrieving data
-        retrieveDataFromFirestore()
+        // Test Firestore connection by retrieving data and performing field rename
+        GlobalScope.launch(Dispatchers.IO) {
+            renameFieldInCollection()
+        }
     }
 
-    private fun retrieveDataFromFirestore() {
-        db.collection("menu")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    // Enumerate all fields in the document
-                    for ((field, value) in document.data) {
-                        Log.d("FirestoreTest", "Document ID: ${document.id}, Field: $field, Value: $value")
+    private suspend fun renameFieldInCollection() {
+        try {
+            val querySnapshot = db.collection(collectionName).get().await()
 
-//                        // Update TextView with retrieved data
-//                        textView.append("$field: $value\n")
-                    }
+            for (document in querySnapshot.documents) {
+                // Rename the field in each document
+                val data = document.data
+                if (data != null && data.containsKey("imageUrl")) {
+                    val newValue = data["imageUrl"]
+                    data.remove("imageUrl")
+                    data["photoUrl"] = newValue
+
+                    // Update the document with the new data
+                    db.collection(collectionName).document(document.id).set(data).await()
                 }
             }
-            .addOnFailureListener { exception ->
-                Log.e("FirestoreTest", "Error getting documents: ", exception)
-            }
+
+            Log.d("FirestoreTest", "Field rename complete.")
+        } catch (e: Exception) {
+            Log.e("FirestoreTest", "Error during field rename: $e")
+        }
     }
 }
